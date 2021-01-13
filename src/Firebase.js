26ed94f6f6343d87972040074ab31143e21c0f03
Patrik Souza <doublecrt@gmail.com>
2021-01-13 16:23:06 -0300

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,11 +4,13 @@ import firestore from 'firebase/firestore'
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET"
+  apiKey: "AIzaSyBYC1YghWOLQ1m9Nad8SaP14ja4Kz-E9BQ",
+    authDomain: "reactjs-test-2b646.firebaseapp.com",
+    projectId: "reactjs-test-2b646",
+    storageBucket: "reactjs-test-2b646.appspot.com",
+    messagingSenderId: "196752635752",
+    appId: "1:196752635752:web:2546594f7f7274a96c48dd",
+    
 };
 firebase.initializeApp(config);
 
